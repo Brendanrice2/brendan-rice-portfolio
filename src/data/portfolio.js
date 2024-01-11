6b74087a7e3d25fc18060dbc1137f96f72e5c1b4
Brendanrice2 <90341941+Brendanrice2@users.noreply.github.com>
2024-01-10 19:26:58 -0500

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Sports Analytics and Statistics Site',
-        imgUrl: 'assets/Fantasy_Site_img.png',
+        imgUrl: '/assets/Fantasy_Site_img.png',
         stack: ['HTML', 'CSS', 'JavaScript'],
         link: 'https://brendanrice2.github.io/Fantasy_Football_2023.github.io/'
     },
     {
         title: 'Developer Portfolio',
-        imgUrl: 'assets/portfolio_img.png',
+        imgUrl: '/assets/portfolio_img.png',
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://brendanrice2.github.io/brendan-rice-portfolio/'
     },
     {
         title: 'Operating Systems Final Project',
-        imgUrl: 'assets/412_final_img.png',
+        imgUrl: '/assets/412_final_img.png',
         stack: ['C', 'C++'],
         link: 'https://github.com/Brendanrice2/412-Final-Project'
     },
