90318c9e177ca8a7fcd4e4cb610574f22cbe9548
Brendan Rice <brendan_rice@uri.edu>
2024-02-29 12:34:36 -0500

    Hopefully - praying - that I fixed the image paths for distribution

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,20 @@
 export default [
     {
         title: 'Sports Analytics and Statistics Site',
-        imgUrl: './public/assets/fantasySiteImg.png',
+        imgUrl: 'assets/fantasySiteImg.png',
         stack: ['HTML', 'CSS', 'JavaScript'],
         link: 'https://brendanrice2.github.io/Fantasy_Football_2023.github.io/'
     },
     {
         title: 'Developer Portfolio',
-        imgUrl: './public/assets/portfolioImg.png',
+        imgUrl: 'assets/portfolioImg.png',
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://brendanrice2.github.io/brendan-rice-portfolio/'
     },
     {
         title: 'Operating Systems Final Project',
-        imgUrl: 'public/assets/412FinalImg.png',
+        imgUrl: 'assets/412FinalImg.png',
         stack: ['C', 'C++'],
         link: 'https://github.com/Brendanrice2/412-Final-Project'
-    },
+    }
 ]
