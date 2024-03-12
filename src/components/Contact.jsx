d4622b05443590dd582f07da533283c436880d2d
Brendan Rice <brendan_rice@uri.edu>
2024-03-12 15:43:23 -0400

    Removed copy and paste hyperlink for my email, because execCommand function was deprecated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,16 +3,6 @@ import Title from './Title';
 
 function Contact() {
 
-    function copyEmailToClipboard(email) {
-        var dummy = document.createElement("textarea");
-        document.body.appendChild(dummy);
-        dummy.value = email;
-        dummy.select();
-        document.execCommand("copy");
-        document.body.removeChild(dummy);
-        alert("Email copied to clipboard: " + email);
-    }
-
     return (
         <div className="flex flex-col mb-10 mx-auto">
             <div className='flex justify-center
@@ -24,8 +14,6 @@ function Contact() {
             font-bold">I'm always open to new opportunities, and I'm always looking to learn something new. If you have a project that you'd like to work with me on, or if you'd like to get in touch, please feel free to reach out to me via my email: {' '}
     
                         <a
-                            href="#"
-                            onclick={() => copyEmailToClipboard('brendanjrice32@gmail.com')}
                             className="text-cyan-600 hover:underline
                             underline-offset-2 decoration-2
                             decoration-orange-600">
